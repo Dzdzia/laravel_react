e82d83f047c75c1871bd6c2e414fe5dd95f2cb04
Mikołaj Stępień <piomik98@gmail.com>
2023-05-24 22:32:45 +0200

    Dodanie logowania/rejestracji przy pomocy breeze/blade

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
-        reactRefresh(),
     ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src_react'),
-        },
-    },
-    build: {
-        outDir: 'public/js',
-        assetsDir: '',
-        rollupOptions: {
-            input: {
-                main: path.resolve(__dirname, 'src_react/main.jsx'),
-            },
-            output: {
-                entryFileNames: 'main.js',
-                chunkFileNames: 'main.js',
-                assetFileNames: 'main.js',
-            },
-        },
-    },
 });
